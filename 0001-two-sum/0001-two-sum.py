b6e16c99135ab0bad5fa4bfd4eb9c09bfdd88de3
Henam Singla <56500520+HenamSingla@users.noreply.github.com>
2025-03-25 09:10:03 -0500

    Time: 0 ms (100%), Space: 19.2 MB (11.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,21 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # for num in nums:
-        #     remaining = target - num
-        #     if remaining in nums and nums.index(num) != nums.index(remaining):
-        #         return [nums.index(num), nums.index(remaining)]
-        
-        
-        dictionary = {}
-        for i, num in enumerate(nums):
-            remain = target - num
-            if num in dictionary.keys():
-                return[i,dictionary[num]]
+        ###NEE TO USE A DICTIONARY
+        h_map ={}
+        for i,num in enumerate(nums):
+            comp= target-num
+            if comp in h_map.keys():
+                return [h_map[comp],i]
+            else:
+                h_map[num]=i
 
-            dictionary[remain] = i
-            # print(i)
-            # print(dictionary)
-
-            
-            
-                
+        
