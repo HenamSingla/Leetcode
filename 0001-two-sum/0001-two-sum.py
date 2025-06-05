5c4d3efcad075b095a0927a8d2d0a55d215140d3
Henam Singla <singlahenam45@gmail.com>
2025-06-05 15:43:59 -0500

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -9,4 +9,18 @@ class Solution:
             else:
                 h_map[num]=i
 
-        
+
+
+
+### Neetcode JUNE 5
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen={}
+        for i,num in enumerate(nums):
+            diff=target-num
+            if diff in seen:
+                return [seen[diff],i]
+            else:
+                seen[num]=i
+            
