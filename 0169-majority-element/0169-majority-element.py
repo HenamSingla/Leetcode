6b6302459dd8fa735f0001791fd21825e5d1d2c9
Henam Singla <singlahenam45@gmail.com>
2026-01-29 13:00:48 -0500

    Time: 7 ms (57.92%), Space: 21.2 MB (20.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,16 +1,34 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         #first thought is to use hash map and the max value in the keys is the answer 
-        hashmap={}
-        for i,num in enumerate(nums):
-            if num not in hashmap:
-                hashmap[num]=1
-            else:
-                hashmap[num]+=1
-        print(hashmap)
-        max_value=max(hashmap.values())
-        print(max_value)
+        # hashmap={}
+        # for num in nums:
+        #     if num not in hashmap:
+        #         hashmap[num]=1
+        #     else:
+        #         hashmap[num]+=1
+        # print(hashmap)
+        # max_value=max(hashmap.values())
+        # print(max_value)
         
-        for key,value in hashmap.items():
-            if value==max_value:
-                return key
+        # for key,value in hashmap.items():
+        #     if value==max_value:
+        #         return key
+#Time: O(n)
+#Space: Hashmap (O(n)) (worst case all diff pairs) 
+
+
+###Boyer-Moore voting 
+        count=0
+        res=0
+        for num in nums:
+            if count==0:
+                res=num
+            if res==num:
+                count+=1
+            else:
+                count-=1
+        return res
+
+
+
