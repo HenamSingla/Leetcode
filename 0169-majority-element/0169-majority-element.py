2ef0adf9467258b7074b098603d2a8960fde2641
Henam Singla <singlahenam45@gmail.com>
2026-01-29 10:51:04 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        #first thought is to use hash map and the max value in the keys is the answer 
+        hashmap={}
+        for i,num in enumerate(nums):
+            if num not in hashmap:
+                hashmap[num]=1
+            else:
+                hashmap[num]+=1
+        print(hashmap)
+        max_value=max(hashmap.values())
+        print(max_value)
+        
+        for key,value in hashmap.items():
+            if value==max_value
+            return key
