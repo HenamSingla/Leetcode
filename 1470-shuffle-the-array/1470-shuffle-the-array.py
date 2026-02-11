6b0217622f640920051879c9d31645a93975622f
Henam Singla <singlahenam45@gmail.com>
2026-02-11 14:37:42 -0500

    Time: 57 ms (75.12%), Space: 19.4 MB (37.13%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,10 +1,29 @@
+# class Solution:
+#     def shuffle(self, nums: List[int], n: int) -> List[int]:
+        # res=[0]*(2*n)
+        # for i in range(n):
+        #     #0,1,2
+        #     res[2*i]=nums[i]
+        #     #res[0,2,4]
+        #     res[2*i+1]=nums[n+i]
+        #     #res[1,3,5]
+        # return res
+
+
+
+
+
+
+
+
+
+
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        res=[0]*(2*n)
-        for i in range(n):
-            #0,1,2
-            res[2*i]=nums[i]
-            #res[0,2,4]
-            res[2*i+1]=nums[n+i]
-            #res[1,3,5]
+        i=0
+        res=[]
+        while i<n:
+            res.append(nums[i])
+            res.append(nums[i+n])
+            i+=1
         return res
